baef8ee73ecd027256147f75fa7581af92276399
AndrianinaHa <yankeenantsou@gmail.com>
2021-07-20 19:09:30 +0300

    A blog about an anime

---
+++ app/includes/footer.php
@@ -0,0 +1,63 @@
+ <!-- footer -->
+ <div class="footer">
+    <div class="footer-content">
+
+      <div class="footer-section about">
+        <h1 class="logo-text"><span>Anime</span>-GC</h1>
+        <p>
+          Anime-gc is a blog where there are many facts and memes of the anime Guilty crown
+        </p>
+        <div class="contact">
+          <span><i class="fas fa-phone"></i> &nbsp; 020-22-321-32</span>
+          <span><i class="fas fa-envelope"></i> &nbsp; anime-gc@fact.com</span>
+        </div>
+        <div class="socials">
+          <a href="#"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+
+      <div class="footer-section links">
+        <h2>Quick Links</h2>
+        <br>
+        <ul>
+          <a href="#">
+            <li>Events</li>
+          </a>
+          <a href="#">
+            <li>Team</li>
+          </a>
+          <a href="#">
+            <li>Mentores</li>
+          </a>
+          <a href="#">
+            <li>Gallery</li>
+          </a>
+          <a href="#">
+            <li>Terms and Conditions</li>
+          </a>
+        </ul>
+      </div>
+
+      <div class="footer-section contact-form">
+        <h2>Contact us</h2>
+        <br>
+        <form action="index.html" method="post">
+          <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+          <button type="submit" class="btn btn-big contact-btn">
+            <i class="fas fa-envelope"></i>
+            Send
+          </button>
+        </form>
+      </div>
+
+    </div>
+
+    <div class="footer-bottom">
+      &copy; Designed solely by me
+    </div>
+  </div>
+  <!-- // footer -->
